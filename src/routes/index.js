945b51f6da677d9d754c89d5399e203e0b984e4a
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2020-10-22 20:32:04 +1100

    refactor port for deployment

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,10 @@ const routes = require('express').Router();
 
 const showsRouter = require('./shows.routes');
 
+routes.get('/', (req, res) => {
+  res.send('Welcome to my resolution of the coding challenge. To access the api endpoint for shows, please add "/api/shows" to the end of the url.')
+});
+
 routes.use('/api/shows', showsRouter);
 
 module.exports = routes;
