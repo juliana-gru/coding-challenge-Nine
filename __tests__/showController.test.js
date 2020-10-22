36b41c962676a63aee4368f586ad25cab472b868
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2020-10-22 18:04:36 +1100

    add ShowController tests

---
+++ __tests__/showController.test.js
@@ -0,0 +1,32 @@
+const request = require('supertest');
+
+const app = require('../src/app');
+const exampleRequest = require('./utils/exampleRequest.json');
+const invalidRequest  = require('./utils/invalidRequest.json');
+
+
+describe('ShowController', () => {
+  it('should receive a valid payload and succesfully return eligible shows', async () => {
+    const response = await request(app)
+    .post('/api/shows')
+    .send(exampleRequest)
+
+    const responseArray = response.body.response;
+
+    expect(response.status).toBe(200);
+    expect(responseArray.length).toBe(7);
+  })
+});
+
+describe('ShowController', () => {
+  it('should return an error when invalid JSON data is received', async () => {
+    const response = await request(app)
+    .post('/api/shows')
+    .send(invalidRequest);
+
+    expect(response.status).toBe(400);
+    expect(response.error).toBe(`{
+      "error":"Could not decode request: JSON parsing failed"
+    }`) 
+  })
+})
