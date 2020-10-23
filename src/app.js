447f6154c187508c97ccb821bd10eeaa8deef278
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2020-10-23 14:05:15 +1100

    add middleware to handle 404 requests

--- src/app.js
+++ src/app.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+app.use((req, res, next) => {
+  return res.status(404).json({error: "Nothing to see here"});
+});
+
 module.exports = app;
