f5f96505fe1e5d4e330f65024df4911417a784a6
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2020-10-23 13:22:45 +1100

    update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const routes = require('express').Router();
 const showsRouter = require('./shows.routes');
 
 routes.get('/', (req, res) => {
-  res.json('Welcome! To access the api endpoint for shows, please add /api/shows to the end of the url.')
+  res.json({ message: 'Welcome! To access the api endpoint for shows, please add /api/shows to the end of the url.' })
 });
 
 routes.use('/api/shows', showsRouter);
